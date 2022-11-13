afce12cae63003097cea2709c2c9597862d1bc9e
Admin <dmtr2001@mail.ru>
2022-11-13 21:39:03 +0700

    commit 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,15 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-    
+        String[] nameBook  = new  String[2];
+
+        nameBook[0] = " Гарри Поттер Джоан Роулинг";
+        nameBook[1] = " Шерлок Холмс Артура Конан Дойл";
+
+
+
+
+
+
     }
 }
--- src/com/company/book.java
+++ src/com/company/book.java
@@ -2,4 +2,17 @@ package com.company;
 
 public class book {
 
+   private String nameBook;
+    private String nameAutor;
+
+    public  book (String nameBook, String nameAutor){
+        this.nameBook = nameAutor;
+        this.nameAutor = nameBook;
+
+    }
+
+    @Override
+    public String toString() {
+        return nameAutor + " " + nameAutor;
+    }
 }
