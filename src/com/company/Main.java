7d398eff32ed43ee4781d825e61f59d54647d444
Admin <dmtr2001@mail.ru>
2022-11-16 00:04:04 +0700

    сcommit2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,16 +2,16 @@ package com.company;
 
 public class Main {
 
+   // book[] towBook = new book[10];
+
     public static void main(String[] args) {
-        String[] nameBook  = new  String[2];
-
-        nameBook[0] = " Гарри Поттер Джоан Роулинг";
-        nameBook[1] = " Шерлок Холмс Артура Конан Дойл";
-
-
-
-
-
+        book[] towBook = new book[2];
+        towBook[0] = new book("Гарри Поттер","Джоан Роулинг");
+        towBook[1] = new book("Зеленая миля","Стивен Кинг");
+        for (book book : towBook) {
+            System.out.println(book.toString());
+        }
 
     }
 }
+
